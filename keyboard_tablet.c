16d7cc75389278c4c9a9574375ea9015af511ae0
Willsem <happymainer@gmail.com>
2020-11-21 14:42:11 +0300

    :pencil: add probe function

--- keyboard_tablet.c
+++ keyboard_tablet.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/usb/input.h>
+#include <linux/slab.h>
 
 #define DRIVER_NAME    "keyboard_tablet"
 #define DRIVER_AUTHOR  "Alexander Stepanov"
@@ -15,28 +16,112 @@ MODULE_LICENSE(DRIVER_LICENSE);
 #define ID_VENDOR_TABLET  0x056a /* Wacom Co. */
 #define ID_PRODUCT_TABLET 0x0301 /* Ltd CTL-671 */
 
+#define USB_PACKET_LEN  10
+#define WHEEL_THRESHOLD 4
+
 struct tablet_features {
     int pkg_len;
 };
 
 struct tablet {
-    unsigned char          *data;
-    struct tablet_features *features;
+    unsigned char     *data;
+    dma_addr_t         data_dma;
+    struct input_dev  *input_dev;
+    struct usb_device *usb_dev;
+    struct urb        *irq;
+    int                old_wheel_pos;
+    char               phys[32];
 };
 
+typedef struct tablet tablet_t;
+
+static int tablet_open(struct input_dev *dev) {
+    tablet_t *tablet = input_get_drvdata(dev);
+
+    tablet->old_wheel_pos = -WHEEL_THRESHOLD - 1;
+    tablet->irq->dev = tablet->usb_dev;
+    if (usb_submit_urb(tablet->irq, GFP_KERNEL))
+        return -EIO;
+
+    return 0;
+}
+
+static void tablet_close(struct input_dev *dev) {
+    tablet_t *tablet = input_get_drvdata(dev);
+    usb_kill_urb(tablet->irq);
+}
+
 static int tablet_probe(struct usb_interface *interface, const struct usb_device_id *id) {
+    struct usb_device *usb_device = interface_to_usbdev(interface);
+    tablet_t *tablet;
+    struct input_dev *input_dev;
+    int error = -ENOMEM;
+
+    printk(KERN_INFO "[%s] probe checking tablet\n", DRIVER_NAME);
+
+    tablet = kzalloc(sizeof(tablet_t), GFP_KERNEL);
+    input_dev = input_allocate_device();
+    if (!tablet || !input_dev) {
+        input_free_device(input_dev);
+        kfree(tablet);
+        return error;
+    }
+
+    tablet->data = (unsigned char *)usb_alloc_coherent(usb_device, USB_PACKET_LEN, GFP_KERNEL, &tablet->data_dma);
+    if (!tablet->data) {
+        input_free_device(input_dev);
+        kfree(tablet);
+        return error;
+    }
+
+    tablet->irq = usb_alloc_urb(0, GFP_KERNEL);
+    if (!tablet->irq) {
+        usb_free_coherent(usb_device, USB_PACKET_LEN, tablet->data, tablet->data_dma);
+        input_free_device(input_dev);
+        kfree(tablet);
+        return error;
+    }
+
+    tablet->usb_dev = usb_device;
+    tablet->input_dev = input_dev;
+
+    usb_make_path(usb_device, tablet->phys, sizeof(tablet->phys));
+    strlcat(tablet->phys, "/input0", sizeof(tablet->phys));
+
+    input_dev->name = DRIVER_NAME;
+    input_dev->phys = tablet->phys;
+    usb_to_input_id(usb_device, &input_dev->id);
+    input_dev->dev.parent = &interface->dev;
+
+    input_set_drvdata(input_dev, tablet);
+
+    input_dev->open = tablet_open;
+    input_dev->close = tablet_close;
+
+    // code here
+
+    error = input_register_device(tablet->input_dev);
+    if (error) {
+        usb_free_urb(tablet->irq);
+        usb_free_coherent(usb_device, USB_PACKET_LEN, tablet->data, tablet->data_dma);
+        input_free_device(input_dev);
+        kfree(tablet);
+        return error;
+    }
+
+    usb_set_intfdata(interface, tablet);
     return 0;
 }
 
 static void tablet_disconnect(struct usb_interface *interface) {
-    struct tablet *tablet = usb_get_intfdata(inteface);
-    usb_set_intfdata(inteface, NULL);
+    tablet_t *tablet = usb_get_intfdata(interface);
+    usb_set_intfdata(interface, NULL);
 
     if (tablet) {
         usb_kill_urb(tablet->irq);
-        input_unregister_device(tablet->dev);
+        input_unregister_device(tablet->input_dev);
         usb_free_urb(tablet->irq);
-        usb_free_coherent(interface_to_usbdev(interface), tablet->features->pkg_len, tablet->data, tablet->data_dma);
+        usb_free_coherent(interface_to_usbdev(interface), USB_PACKET_LEN, tablet->data, tablet->data_dma);
         kfree(tablet);
     }
 }
@@ -49,7 +134,7 @@ static struct usb_device_id tablet_table [] = {
 MODULE_DEVICE_TABLE(usb, tablet_table);
 
 static struct usb_driver tablet_driver = {
-    .name       = "keyboard_tablet",
+    .name       = DRIVER_NAME,
     .probe      = tablet_probe,
     .disconnect = tablet_disconnect,
     .id_table   = tablet_table,
